0820abfe686e6d173cf4b7cf48ed602b18ce1921
Emma Samuel <emma.e.samuel@gmail.com>
2022-04-20 12:59:47 +0100

    Create hello_world.rb

---
+++ makers-course/week-1/hello_world.rb
@@ -0,0 +1,7 @@
+puts 1+1
+
+#Student@MA121 week-1 % irb -r ./hello_world.arb
+#/Users/Student/.rvm/rubies/ruby-3.0.0/lib/ruby/3.0.0/irb/init.rb:366: warning: LoadError: cannot load such file -- ./hello_world.arb
+
+#ruby ./hello_world.arb
+#2
