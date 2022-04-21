514599ea0bcdfc8242d4c856be9a55fe6d37f7a4
Emma Samuel <emmameaden95@gmail.com>
2022-04-21 11:41:53 +0100

    Update variables.rb

--- makers-course/week-1/variables.rb
+++ makers-course/week-1/variables.rb
@@ -1,10 +1,12 @@
-ONE = 1
-TWO = 2
-THREE = 3
-FOUR = 4
-FIVE = 5
-SIX = 6
-SEVEN = 7
-EIGHT = 8
-NINE = 9
-TEN = 10
+one = 1.to_f
+two = 2.to_f
+three = 3.to_f
+four = 4.to_f
+five = 5.to_f
+six = 6.to_f
+seven = 7.to_f
+eight = 8.to_f
+nine = 9.to_f
+ten = 10.to_f
+
+puts four / five
