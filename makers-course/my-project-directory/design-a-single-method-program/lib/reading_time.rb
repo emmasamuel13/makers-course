bc7eb3bb1e013054ad67c8e53181231c0710a410
Emma Samuel <emmameaden95@gmail.com>
2022-05-03 17:23:47 +0100

    Update reading_time.rb

--- makers-course/my-project-directory/design-a-single-method-program/lib/reading_time.rb
+++ makers-course/my-project-directory/design-a-single-method-program/lib/reading_time.rb
@@ -3,10 +3,13 @@
 #I want to see an estimate of reading time for a text, assuming that I can read 200 words a minute.
 
 def reading_time(text_length)
-  mins = text_length / 200
-  if text_length % 200 == 0
-    return "It will take you #{mins} minutes to read this text"
+  if text_length.to_i != text_length
+    fail "That's not an integer"
+  elsif text_length < 0
+    fail "Number needs to be positive"
+  elsif text_length % 200 == 0
+    return "It will take you #{text_length / 200} minutes to read this text"
   else
-    return "It will take you #{mins + 1} minutes to read this text"
+    return "It will take you #{(text_length / 200) + 1} minutes to read this text"
   end
 end
