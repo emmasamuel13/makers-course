11ebf294be5f221bb2047a95a2cd4160e43d87b2
Emma Samuel <emmameaden95@gmail.com>
2022-05-05 11:53:45 +0100

    Update diary_entry.rb

--- golden_square/test_drive_a_single_class_program/lib/diary_entry.rb
+++ golden_square/test_drive_a_single_class_program/lib/diary_entry.rb
@@ -2,23 +2,29 @@ class DiaryEntry
   def initialize(title, contents) # title, contents are strings
     @title = title
     @contents = contents
+    @end_of_reading = 0
   end
+
   def title
     return @title
   end
+
   def contents
     return @contents
   end
+
   def count_words
     # Returns the number of words in the contents as an integer
     return @contents.split(" ").length
   end
+
   def reading_time(wpm) # wpm is an integer representing the number of words the
                         # user can read per minute
     # Returns an integer representing an estimate of the reading time in minutes
     # for the contents at the given wpm.
     return count_words / wpm
   end
+
   def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
                                   # of words the user can read per minute
                                   # `minutes` is an integer representing the
@@ -28,15 +34,13 @@ class DiaryEntry
     # If called again, `reading_chunk` should return the next chunk, skipping
     # what has already been read, until the contents is fully read.
     # The next call after that it should restart from the beginning.
-    reading = []
-    count = 1
-    chunk = @contents.split(" ")
-    while count <= minutes do
-      reading.push(chunk[0..wpm - 1])
-      chunk.push(chunk[0..wpm - 1].join(" "))
-      to_delete = [0..wpm - 1]
-      chunk.delete_if.with_index{|_, index| to_delete.include?(index)}
-      count += 1
+    contents = @contents.split(" ")
+    amount_can_read = wpm * minutes
+    read_upto = @end_of_reading
+    @end_of_reading += amount_can_read
+    reading = contents[read_upto..@end_of_reading - 1]
+    if @end_of_reading >= contents.length
+      @end_of_reading = 0
     end
     return reading.join(" ")
   end
