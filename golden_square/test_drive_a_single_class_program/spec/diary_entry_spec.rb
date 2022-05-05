b098b0292733ded38ecbf3315b83330f5c99e1b4
Emma Samuel <emmameaden95@gmail.com>
2022-05-05 11:55:46 +0100

    Update diary_entry_spec.rb

--- golden_square/test_drive_a_single_class_program/spec/diary_entry_spec.rb
+++ golden_square/test_drive_a_single_class_program/spec/diary_entry_spec.rb
@@ -1,5 +1,5 @@
-
 require "diary_entry"
+
 RSpec.describe DiaryEntry do
   context "initialize diary entry" do
     it "initializes and returns title and contents" do
@@ -8,6 +8,7 @@ RSpec.describe DiaryEntry do
       expect(diary_entry.contents).to eq "Learning is fun!"
     end
   end
+
   context "returns the total number of words in the contents" do
     it "returns 3" do
       diary_entry = DiaryEntry.new("Makers", "Learning is fun!")
@@ -15,6 +16,7 @@ RSpec.describe DiaryEntry do
       expect(result).to eq 3
     end
   end
+
   context "returns how many minutes it will take to read a text" do
     it "returns 3" do
       diary_entry = DiaryEntry.new("Makers", "Learning is fun!")
@@ -22,11 +24,26 @@ RSpec.describe DiaryEntry do
       expect(result).to eq 3
     end
   end
+
   context " return how many words can be read in the given time" do
-    it "return how many words can be read in the given time" do
+    it "returns -Learning is-" do
       diary_entry = DiaryEntry.new("Makers", "Learning is fun!")
       result = diary_entry.reading_chunk(1, 2)
       expect(result).to eq "Learning is"
+    end
+
+    it "returns -Learning is fun!-" do
+      diary_entry = DiaryEntry.new("Makers", "Learning is fun!")
+      result = diary_entry.reading_chunk(3, 3)
+      expect(result).to eq "Learning is fun!"
+    end
+
+    it "returns -Learning is- -fun!-" do
+      diary_entry = DiaryEntry.new("Makers", "Learning is fun!")
+      result = diary_entry.reading_chunk(2, 1)
+      expect(result).to eq "Learning is"
+      result = diary_entry.reading_chunk(2, 1)
+      expect(result).to eq "fun!"
     end
   end
 end
