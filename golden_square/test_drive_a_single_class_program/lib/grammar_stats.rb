b5c9aa755f4fe6935377b0c3db3fbcaee5e63dda
Emma Samuel <emmameaden95@gmail.com>
2022-05-05 17:55:23 +0100

    Update grammar_stats.rb

--- golden_square/test_drive_a_single_class_program/lib/grammar_stats.rb
+++ golden_square/test_drive_a_single_class_program/lib/grammar_stats.rb
@@ -22,6 +22,8 @@ class GrammarStats
   end
 
   def percentage_good
+    # Returns as an integer the percentage of texts checked so far that passed
+    # the check defined in the `check` method. The number 55 represents 55%.
     percent = @passes.to_f / @attempts.to_f * 100
     return percent.to_i
   end
