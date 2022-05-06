54c468869eac1c4c34ec410e4bb637157f147fe0
Emma Samuel <emmameaden95@gmail.com>
2022-05-06 13:04:58 +0100

    Update task_tracker.rb

--- golden_square/design-a-single-class-program/lib/task_tracker.rb
+++ golden_square/design-a-single-class-program/lib/task_tracker.rb
@@ -8,8 +8,7 @@
 
 class TaskTracker
   def initialize(tasks)
-    @task_list = []
-    @task_list << tasks.split(", ")    
+    @task_list = tasks.split(", ")
   end
 
   def add(task) # adds the task to a list
@@ -19,9 +18,7 @@ class TaskTracker
 
   def complete(task) # marks the task as complete and removes it from the list
     new_list = []
-    puts "here is task #{task}"
     @task_list.each do |chore|
-      puts "here is chore #{chore}"
       unless chore == task
         new_list << chore
       end
@@ -29,6 +26,3 @@ class TaskTracker
     return new_list.join("\n")
   end
 end
-
-taskTracker = TaskTracker.new("feed the cats, take medication, empty washing machine, walk the dog")
-puts taskTracker.complete("take medication")
