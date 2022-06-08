63d08ae6b5a187249c5dd472ba2856fb39cff521
Emma Samuel <emmameaden95@gmail.com>
2022-06-08 14:49:07 +0100

    price display

---
+++ learning_java_vs_code/price_display.java
@@ -0,0 +1,10 @@
+package learning_java_vs_code;
+
+class PriceDisplay {
+  public static void main(String[] args) {
+    String item = "coffee machine";
+    double price = 15.99;
+
+    System.out.printf("The price of the %s is %s\n", item, price);
+  }  
+}
